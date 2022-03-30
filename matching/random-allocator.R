0673151aca36b8cdfc9d2923864818f5f9ff60b9
Kennyanthro <adam.kenny@anthro.ox.ac.uk>
2022-03-30 12:19:25 +0100

    Added code to generate random allocator from final pilot study, using previewed data.

--- matching/random-allocator.R
+++ matching/random-allocator.R
@@ -26,7 +26,6 @@ default_donation <- 10 # represents half the points
 ## NB if necessary, nationalities could use language codes from qualtrics:
 ## https://www.qualtrics.com/support/survey-platform/survey-module/survey-tools/translate-survey/#AvailableLanguageCodes
 
-
 ## create tibble
 random_allocator <-
 
@@ -42,7 +41,7 @@ random_allocator <-
     )
 
 ##################################################
-## create tibble using pilot data
+## create tibble using initial pilot data (Flurp and Zazz)
 
 ## read data
 ## FIXME upload pilot data if it can be shared
@@ -107,6 +106,57 @@ random_allocator <-
     slice_sample(df, n = 1)
 
 ##################################################
+## create tibble using final pilot data (2022-03)
+
+## read data
+## FIXME upload pilot data if it can be shared
+## alternatively, manually place csv file in ./data
+df <-
+    
+    read_csv("../data/PSA-MGP Prolific pilot (US + UK)_March 30, 2022_02.11.csv") %>%
+    ## remove first lines which are qualtrics bumf
+    slice(-(1:3)) %>%
+    ## allocation decisions
+    select(
+        ## info on nationality group
+        nationality,
+        ## info on minimal group
+        ingroupletter,
+        ## dg_min
+        Allocate_IG_M_1, Allocate_IG_M_2,
+        Allocate_OG_M_1, Allocate_OG_M_2,
+        Allocate_TG_M_1, Allocate_TG_M_2,
+        Allocate_IG_P_1, Allocate_IG_P_2,
+        Allocate_OG_P_1, Allocate_OG_P_2,
+	Allocate_TG_P_1, Allocate_TG_P_2,
+        ## dg_nat
+        Allocate_IG_Nat_1, Allocate_IG_Nat_2,
+        Allocate_OG_Nat_1, Allocate_OG_Nat_2,
+        Allocate_TG_Nat_1, Allocate_TG_Nat_2) %>% 
+    mutate(
+        ## merge minimal decisions
+        Allocate_IG_M = if_else(is.na(Allocate_IG_M_1), Allocate_IG_P_1, Allocate_IG_M_1),
+        Allocate_OG_M = if_else(is.na(Allocate_OG_M_1), Allocate_OG_P_1, Allocate_OG_M_1),
+        Allocate_TG_M = if_else(is.na(Allocate_TG_M_1), Allocate_TG_P_1, Allocate_TG_M_1)) %>%
+        ## rename columns
+    mutate(minimal = ingroupletter,
+           ## FIXME double check that above manipulation tracks
+           dg_min_in_self = as.numeric(Allocate_IG_M),
+           dg_min_out_self = as.numeric(Allocate_OG_M),
+           dg_min_in_out = as.numeric(Allocate_TG_M),
+           dg_nat_in_self = as.numeric(Allocate_IG_Nat_1),
+           dg_nat_out_self = as.numeric(Allocate_OG_Nat_1),
+           dg_nat_in_out = as.numeric(Allocate_TG_Nat_1)) %>%
+    select(minimal, nationality, ends_with("_q"), starts_with("dg_")) %>%
+    ## FIXME in case there are NAs in allocations, replace with midpoint
+    mutate_at(vars(starts_with("dg_")), ~replace_na(., 0))
+
+## randomly sample one row
+random_allocator <-
+
+    slice_sample(df, n = 1)
+
+##################################################
 ## convert to json and export
 
 ## create json
@@ -119,3 +169,5 @@ df_as_json <-
 writeLines(df_as_json,
            paste0("random-allocator.json") # FIXME per nation?
            )
+
+
