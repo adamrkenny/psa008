23d5741c861f145213234db2f9d27a7998fa51eb
Kennyanthro <adam.kenny@anthro.ox.ac.uk>
2024-02-05 14:56:02 +0000

    Fixed typo and made code iterative.

--- analysis/analysis-power-r5.R
+++ analysis/analysis-power-r5.R
@@ -1,8 +1,3 @@
-
-## NB I've removed the forloop here so that you can check that the
-## code runs sensibly, providing default values of countries (40),
-## participants per country (200), etc
-
 ## packages
 library(tidyverse)
 library(simr)
@@ -10,19 +5,12 @@ library(lmerTest)
 library(furrr)
 
 ## simulation parameters
-K <- 20 # number of countries
-N <- 100 # number of participants per country
-
 n_sim <- 10 # set to 1000 for reliable results 
 
 ##################################################
 ## RQ1
 
-## NB these are varied across iterations, here I've provided defaults for code checking 
-beta <- 0.1 # beta for group 
-beta_variance <- 0.01 # c(0.01, 0.02, 0.03) # beta for random slope
-
-## simulation_swarm_rq1 <- function(N, K, beta, beta_variance) {
+simulation_swarm_rq1 <- function(N, K, beta, beta_variance) {
 
     ## set variables for sample
     country <- rep(1:K, each = 2*N) 
@@ -71,42 +59,35 @@ beta_variance <- 0.01 # c(0.01, 0.02, 0.03) # beta for random slope
            class = "rq1"
            )
 
-## }
+}
 
-## ## create tibble with all combinations of parameters
-## parameter_space_rq1 <-
+## create tibble with all combinations of parameters
+parameter_space_rq1 <-
 
-##     crossing(
-##         N = c(100, 150, 200, 250), # number of subjects per country
-##         K = c(30, 40, 50), # number of countries
-##         beta = c(0.1), # beta for group 
-##         beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
-##     )
+    crossing(
+        N = c(100, 150, 200, 250), # number of subjects per country
+        K = c(20, 30, 40, 50), # number of countries
+        beta = c(0.1), # beta for group 
+        beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
+    )
 
-## ## run power analysis across all parameters
-## pwr_rq1 <-
+## run power analysis across all parameters
+pwr_rq1 <-
      
-##     future_pmap_dfr(parameter_space_rq1,
-##                     simulation_swarm_rq1,
-##                     .options = furrr_options(seed = TRUE),
-##                     .progress = TRUE
-##                     )
+    future_pmap_dfr(parameter_space_rq1,
+                    simulation_swarm_rq1,
+                    .options = furrr_options(seed = TRUE),
+                    .progress = TRUE
+                    )
 
-## ## print output
-## print(pwr_rq1)
-
-## ## write output
-## pwr_rq1 %>%
-##     write.csv(file = paste0("./pwr-rq1.csv"))
+## write output
+pwr_rq1 %>%
+    write.csv(file = paste0("./pwr-rq1.csv"))
 
 ##################################################
 ## RQ2
 
-## NB these are varied across iterations, here I've provided defaults for code checking 
-beta <- 0.1 # beta for group 
-beta_variance <- 0.01 # c(0.01, 0.02, 0.03) # beta for random slope
-
-## simulation_swarm_rq2 <- function(N, K, beta, beta_variance) {
+simulation_swarm_rq2 <- function(N, K, beta, beta_variance) {
 
     ## set variables for sample
     country <- rep(1:K, each = 2*N) 
@@ -128,7 +109,7 @@ beta_variance <- 0.01 # c(0.01, 0.02, 0.03) # beta for random slope
         simr::makeLmer(
                   MGE ~ 1 + predictor + (1 + predictor | country),
                   fixef = c(0, beta),
-                  VarCorr = matrix(c(1.5, # var of country random intercept
+                  VarCorr = matrix(c(1.5, # NB var of country random intercept
                                      0, 0, beta_variance),
                                    ncol = 2),
                   sigma = 1,
@@ -149,39 +130,48 @@ beta_variance <- 0.01 # c(0.01, 0.02, 0.03) # beta for random slope
 
     ## create table with powerSim output and info on variables
     summary(sim_rq2) %>%
-    smutate(subjects = N,
+    mutate(subjects = N,
            countries = K,
            beta = beta,
            beta_variance = beta_variance,
            class = "rq2"
            )
 
-## }
+}
 
-## ## create tibble with all combinations of parameters
-## parameter_space_rq2 <-
+## create tibble with all combinations of parameters
+parameter_space_rq2 <-
 
-##     crossing(
-##         N = c(100, 150, 200, 250), # number of subjects per country
-##         K = c(30, 40, 50), # number of countries
-##         beta = c(0.05, 0.10, 0.15), # beta for "predictor" 
-##         beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
-##     )
+    crossing(
+        N = c(100, 150, 200, 250), # number of subjects per country
+        K = c(20, 30, 40, 50), # number of countries
+        beta = c(0.05, 0.10, 0.15), # beta for "predictor" 
+        beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
+    )
 
-## ## write output
-## pwr_rq2 %>%
-##     write.csv(file = paste0("./pwr-rq2.csv"))
+## run power analysis across all parameters
+pwr_rq2 <-
+     
+    future_pmap_dfr(parameter_space_rq2,
+                    simulation_swarm_rq2,
+                    .options = furrr_options(seed = TRUE),
+                    .progress = TRUE
+                    )
+
+## write output
+pwr_rq2 %>%
+    write.csv(file = paste0("./pwr-rq2.csv"))
 
 ##################################################
 ## RQ3
 
 ## NB these are varied across iterations, here I've provided defaults for code checking 
 beta_MGE <- 0.1 # pilot 2: 0.32
-beta_grouptype <- 0.05 # pilot 2: -0.14
+beta_grouptype <- 0.1 # pilot 2: -0.14
 beta_interaction <- 0.1 # pilot 2: 0.11
 beta_variance <- 0.01
 
-## simulation_swarm_rq3 <- function(N, K, beta_interaction, beta_variance) {
+## simulation_swarm_rq3 <- function(N, K, beta_MGE, beta_variance) {
 
     ## set variables for sample
     country <- rep(1:K, each = 2*N) 
@@ -209,9 +199,9 @@ beta_variance <- 0.01
                       (1 + MGE + grouptype | country), 
                   fixef = c(0, # beta for realbias
                             beta_MGE, # beta for MGE
-                            beta_grouptype, # beta for grouptype
-                            beta_interaction), # beta for interaction      
-                  VarCorr = # vc,
+                            0.1, # beta_grouptype, # beta for grouptype
+                            0.1 # beta_interaction), # beta for interaction      
+                  VarCorr = 
                       list(people = 1,
                            country = matrix(c(1,
                                               rep(0, 6),
@@ -221,6 +211,7 @@ beta_variance <- 0.01
                   sigma = 1, 
                   data = df_rq3                 
               )
+
 model_rq3
 
 ## ## you can check interaction plot:
@@ -233,7 +224,9 @@ sjPlot::plot_model(model_rq3, type='int')
             model_rq3,
             test = fcompare(~grouptype), # power for MGE by comparing model without it
             nsim = n_sim,
-            alpha = 0.05/3
+            alpha = 0.05/3,
+            fitOpts = list(control = lmerControl(optCtrl = list(maxfun = 2e5), optimizer = "bobyqa"))
+            # solution for convergence issues https://github.com/pitakakariki/simr/issues/169
       )
 
     ## create table with powerSim output and info on variables
@@ -245,30 +238,30 @@ sjPlot::plot_model(model_rq3, type='int')
            class = "rq3"
            )
 
-## }
+}
 
-## ## create tibble with all combinations of parameters
-## parameter_space_rq3 <-
+## create tibble with all combinations of parameters
+parameter_space_rq3 <-
 
-##     crossing(
-##         N = c(100, 150, 200, 250), # number of subjects per country
-##         K = c(30, 40, 50), # number of countries
-##         beta_interaction = c(0.05, 0.10, 0.15), # beta for "MGE:grouptype1", gone lower as interaction 
-##         beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
-##     )
+    crossing(
+        N = c(100, 150, 200, 250), # number of subjects per country
+        K = c(30, 40, 50), # number of countries
+        beta_interaction = c(0.05, 0.10, 0.15), # beta for "MGE:grouptype1", gone lower as interaction 
+        beta_variance = c(0.01, 0.02, 0.03) # beta for random slope
+    )
 
-## ## run power analysis across all parameters
-## pwr_rq3 <-
+## run power analysis across all parameters
+pwr_rq3 <-
      
-##     future_pmap_dfr(parameter_space_rq3,
-##                     simulation_swarm_rq3,
-##                     .options = furrr_options(seed = TRUE),
-##                     .progress = TRUE
-##                     )
+    future_pmap_dfr(parameter_space_rq3,
+                    simulation_swarm_rq3,
+                    .options = furrr_options(seed = TRUE),
+                    .progress = TRUE
+                    )
 
-## ## print output
-## print(pwr_rq3)
+## print output
+print(pwr_rq3)
 
-## ## write output
-## pwr_rq3 %>%
-##     write.csv(file = paste0("./pwr-rq3.csv"))
+## write output
+pwr_rq3 %>%
+    write.csv(file = paste0("./pwr-rq3.csv"))
